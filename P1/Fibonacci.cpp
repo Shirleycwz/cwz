b089d2d645a0274d1c0e17d18011aec23af97676
Shirley <Shirleycwz@163.com>
2017-06-26 11:00:21 +0800

    修改了代码缩进

--- P1/Fibonacci.cpp
+++ P1/Fibonacci.cpp
@@ -1,20 +1,41 @@
 #include<iostream>
+
 using namespace std;
+
 int main()
+
 {
+	
 	int s;
+	
 	int fi(int);
+	
 	for(int i=1;i<=20;i++)
-	{s=fi(i);
-	cout<<s<<endl;}
-	return 0;	
-}
+	
+	{
+	
+		s=fi(i);
+		
+		cout<<s<<endl;}
+		
+		return 0;
+			
+	}
+	
 int fi(int a)
+
 {
+	
 	int s;
+	
 	if(a<=2)
-	s=1;
+	
+		s=1;
+	
 	else
-	s=(fi(a-1)+fi(a-2));
+	
+		s=(fi(a-1)+fi(a-2));
+	
 	return s;
+	
 }
